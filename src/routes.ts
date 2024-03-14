7edb75fa1fb9fd9df9a16ef25fff5b0a4a27c2dc
Maik2805 <mscardenas77@misena.edu.co>
2024-03-13 22:07:50 -0500

    Implementación Crud Basico Labor

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import * as usuarioController from "./controller/UsuarioController";
 import * as loginController from "./controller/LoginController";
+import * as laborController from "./controller/LaborController";
 
 interface IRoute {
     path: string;
@@ -52,6 +53,36 @@ export const AppRoutes: IRoute[] = [
         path: "/usuarios/inactiveLaborUsuario",
         method: "post",
         action: usuarioController.inactiveLaborUsuario
-    }
+    },
+    {
+        path: "/labores/all",
+        method: "get",
+        action: laborController.all
+    },
+    {
+        path: "/labores/find/:id",
+        method: "get",
+        action: laborController.findById
+    },
+    {
+        path: "/labores/findByUsuario/:id",
+        method: "get",
+        action: laborController.findByUsuario
+    },
+    {
+        path: "/labores/listarInactivasByUsuario/:id",
+        method: "get",
+        action: laborController.listarInactivasByUsuario
+    },
+    {
+        path: "/labores/disponibles",
+        method: "get",
+        action: laborController.listarDisponibles
+    },
+    {
+        path: "/labores",
+        method: "post",
+        action: laborController.save
+    },
 ];
 
