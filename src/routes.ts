a9e0aed5db6b7aa87f79b5b806212656ce3136d7
Maik2805 <mscardenas77@misena.edu.co>
2024-03-13 22:26:24 -0500

    Implementación Crud Básico MedioPago

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import * as usuarioController from "./controller/UsuarioController";
 import * as loginController from "./controller/LoginController";
 import * as laborController from "./controller/LaborController";
+import * as medioPagoController from "./controller/MedioPagoController";
 
 interface IRoute {
     path: string;
@@ -84,5 +85,25 @@ export const AppRoutes: IRoute[] = [
         method: "post",
         action: laborController.save
     },
+    {
+        path: "/medio_pago/all",
+        method: "get",
+        action: medioPagoController.all
+    },
+    {
+        path: "/medio_pago/find/:id",
+        method: "get",
+        action: medioPagoController.findById
+    },
+    {
+        path: "/medio_pago/findByUsuario/:usuario",
+        method: "get",
+        action: medioPagoController.findByUsuario
+    },
+    {
+        path: "/medio_pago",
+        method: "post",
+        action: medioPagoController.save
+    }
 ];
 
