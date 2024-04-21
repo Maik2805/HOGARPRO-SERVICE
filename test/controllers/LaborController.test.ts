37a30322c503d201d49ecff1f46f506707932c99
Maik2805 <mscardenas77@misena.edu.co>
2024-04-21 14:04:17 -0500

    Init Test Estructure

---
+++ test/controllers/LaborController.test.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import { all } from '../../src/controller/LaborController';
+import * as laborService from '../../src/service/LaborService';
+import { Labor } from '../../src/entity/Labor';
+
+jest.mock('../../src/service/LaborService');
+
+describe('LaborController', () => {
+  describe('all', () => {
+    it('should send all labores', async () => {
+      const mockLabores: Labor[] = [
+        {
+          "id": 2,
+          "tipo": "Plomero",
+          "descripcion": "Plomero.",
+          "servicios": [],
+          "laborTrabajador": []
+        },
+        {
+          "id": 1,
+          "tipo": "Paseador de Perros",
+          "descripcion": "Paseador de Perros.",
+          "servicios": [],
+          "laborTrabajador": []
+        }
+      ];
+
+      const mockRequest = {} as Request;
+      const mockResponse = {
+        send: jest.fn(),
+      } as unknown as Response;
+
+      (laborService.all as jest.Mock).mockResolvedValue(mockLabores);
+
+      await all(mockRequest, mockResponse);
+
+      expect(mockResponse.send).toHaveBeenCalledWith(mockLabores);
+    });
+  });
+});
