37a778a5fe2335668b9aabf2dc3bf339d2bb7de7
henlo-mm <esperanza.olivo@correounivalle.edu.co>
2024-05-19 10:58:55 -0500

    Se cambia jest-sonar-reporter por jest-sonar

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,20 @@
 module.exports = {
-    roots: ['<rootDir>/test'],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    testEnvironment: 'node',
-    coverageDirectory: 'coverage',
-    reporters: [
-      "default",
-      [require.resolve('jest-sonar-reporter'), {
-        outputDirectory: 'reports',
-        outputName: 'test-reporter.xml',
-      }]
-    ],
-    collectCoverage: true,
-    coverageReporters: ["lcov", "text", "json", "html"],
+  roots: ['<rootDir>/test'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  reporters: [
+    "default",
+    ["jest-sonar", {
+      outputDirectory: 'reports',
+      outputName: 'test-reporter.xml',
+      reportedFilePath: 'absolute',
+    }]
+  ],
+  collectCoverage: true,
+  coverageReporters: ["lcov", "text", "json", "html"],
 };
