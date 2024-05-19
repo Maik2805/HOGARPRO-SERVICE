2b8230ac0440c3a43a4d701b7b4c14be55a58963
henlo-mm <esperanza.olivo@correounivalle.edu.co>
2024-05-19 10:36:55 -0500

    Implementación de automatización de pruebas con  Sonar y Coverage con Jest

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,15 @@ module.exports = {
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     testEnvironment: 'node',
+    coverageDirectory: 'coverage',
+    testEnvironment: 'node',
+    reporters: [
+      "default",
+      ["jest-sonar", {
+        outputDirectory: 'reports',
+        outputName: 'test-reporter.xml',
+      }]
+    ],
+    collectCoverage: true,
+    coverageReporters: ["lcov", "text", "json", "html"],
 };
